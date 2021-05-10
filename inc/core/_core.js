046464f4d029965e701400fb01841c67b50f6668
andyb <andbrook@me.com>
2021-05-10 17:48:00 +0100

    Fixed navigation behaviour

--- inc/core/_core.js
+++ inc/core/_core.js
@@ -1,3 +1,15 @@
+function externalLinks() { for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) { var b = c[a]; b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank") } } ; externalLinks();
 
+function modifyLinks() {
+   let links = document.getElementsByTagName('a');
+   for (i = 0; i < links.length; i++) {
+      if (links[i].href.endsWith('.pdf')) {
+         links[i].addEventListener('click', function(e) {
+            e.preventDefault();
+            window.open(this.href, '_blank');
+         })
+      }
+   }
+}
 
-
+window.addEventListener('load', modifyLinks);
--- src/js/core/una-navigation.js
+++ src/js/core/una-navigation.js
@@ -1,65 +1,26 @@
-(function($) {
+// Example
+const toggleMenu = document.querySelector('.dropdown-trigger')
+const menu = document.querySelector('.dropdown');
+const closeMenu = document.querySelector(".close");
 
+const toggle = event => {
+   event.stopPropagation();
 
-    $(".dropdown-trigger").on("click", function (event) {
-        event.preventDefault();
-        toggleNav();
-    });
-    $(".dropdown .close").on("click", function (event) {
-        event.preventDefault();
-        toggleNav();
-    });
-    $(".has-children")
-        .children("a")
-        .on("click", function (event) {
-            event.preventDefault();
-            var selected = $(this);
-            selected.next("ul").removeClass("is-hidden").end().parent(".has-children").parent("ul").addClass("move-out");
-        });
-    var submenuDirection = !$(".dropdown-wrapper").hasClass("open-to-left") ? "right" : "left";
-    $(".menu").menuAim({
-        activate: function (row) {
-            $(row).children().addClass("is-active").removeClass("fade-out");
-            if ($(".menu .fade-in").length == 0) $(row).children("ul").addClass("fade-in");
-        },
-        deactivate: function (row) {
-            $(row).children().removeClass("is-active");
-            if ($("li.has-children:hover").length == 0 || $("li.has-children:hover").is($(row))) {
-                $(".menu").find(".fade-in").removeClass("fade-in");
-                $(row).children("ul").addClass("fade-out");
-            }
-        },
-        exitMenu: function () {
-            $(".menu").find(".is-active").removeClass("is-active");
-            return true;
-        },
-        submenuDirection: submenuDirection,
-    });
-    $(".go-back").on("click", function () {
-        var selected = $(this),
-            visibleNav = $(this).parent("ul").parent(".has-children").parent("ul");
-        selected.parent("ul").addClass("is-hidden").parent(".has-children").parent("ul").removeClass("move-out");
-    });
-    function toggleNav() {
-        var navIsVisible = !$(".dropdown").hasClass("dropdown-is-active") ? true : false;
-        $(".dropdown").toggleClass("dropdown-is-active", navIsVisible);
-        $(".dropdown-trigger").toggleClass("dropdown-is-active", navIsVisible);
-        if (!navIsVisible) {
-            $(".dropdown").one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend", function () {
-                $(".has-children ul").addClass("is-hidden");
-                $(".move-out").removeClass("move-out");
-                $(".is-active").removeClass("is-active");
-            });
-        }
+   if (!event.target.closest('.dropdown')) {
 
-        // if (navIsVisible) {
-        //     $(document).keyup(function(e) {
-        //         if(e.which === 27) toggleNav();
-        //     })
-        // }
-    }
+      menu.classList.toggle('dropdown-is-active');
 
-})( jQuery );
+      menu.classList.contains('dropdown-is-active')
+          ? document.addEventListener('click', toggle)
+          : document.removeEventListener('click', toggle);
+   }
+}
+
+toggleMenu.addEventListener('click', toggle);
+closeMenu.addEventListener('click', function(e) {
+   menu.classList.remove("dropdown-is-active");
+   document.removeEventListener('click', toggle);
+});
 
 var menuItems = document.querySelectorAll('#booking li.menu-item-has-children');
 var timer1, timer2;
